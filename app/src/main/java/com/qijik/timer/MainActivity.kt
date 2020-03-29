b44062c8eccf1dbda3c30440bfabe7ffa17db360
Celal ORMANLI <ormanlicelal@gmail.com>
2020-03-29 16:42:11 +0300

    c

--- app/src/main/java/com/qijik/timer/MainActivity.kt
+++ app/src/main/java/com/qijik/timer/MainActivity.kt
@@ -14,29 +14,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var timer1:TextView=findViewById(R.id.timer1)
-        var startValueMinute=0//TODO: Kullanıcıdan alınacak bu değerler
-        var startValueSecond=30
+        var startValueMinute=1//TODO: Kullanıcıdan alınacak bu değerler
+        var startValueSecond=15
         var totalStartValue=startValueMinute*60000+startValueSecond*1000
-        timer1.text = "x.toString()"
-        var count=1
         val timer = object: CountDownTimer(totalStartValue.toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 startValueSecond--
-                timer1.text=startValueMinute.toString()+":"+startValueSecond.toString()
-                if(startValueSecond==0)
+                if(startValueSecond<0)
                 {
                     startValueMinute--
                     startValueSecond=59
                 }
+                if (startValueSecond<10) {
+                    timer1.text = startValueMinute.toString() + ":0" + startValueSecond.toString()
+                }
+                if (startValueMinute<10) {
+                    timer1.text = "0"+startValueMinute.toString() + ":" + startValueSecond.toString()
+                }
+                if (startValueSecond<10 && startValueMinute<10) {
+                    timer1.text = "0"+startValueMinute.toString() + ":0" + startValueSecond.toString()
+                }
+                if (startValueSecond>=10 && startValueMinute>=10) {
+                    timer1.text = startValueMinute.toString() + ":" + startValueSecond.toString()
+                }
+
             }
 
             override fun onFinish() {
                 startValueSecond--
-                timer1.text=startValueMinute.toString()+":"+startValueSecond.toString()
+                timer1.text="0"+startValueMinute.toString()+":0"+startValueSecond.toString()
                 val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)//TODO: Müzik Sesi değiştirmer işlemi yapılacak
                 val r = RingtoneManager.getRingtone(applicationContext,notification)
                 r.play()
-                r
+
             }
         }
         timer.start()
